6a0c8518aaf5fde1d33d3c37c40363147e81b5dc
Getabalew Kemaw <getabalewkemaw@gmail.com>
2026-03-15 23:55:08 +0300

    Time: 0 ms (100%), Space: 12.4 MB (86.05%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums_a=set(nums1)
+        nums_b=set(nums2)
+        return list(nums_a & nums_b)
+
+        
